0b0f26d91c9277f00f12b0df501c23a8a39185de
Gustavo Delfino <gdelfino@mac.com>
2013-07-03 21:20:14 -0430

    Created new Rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parsels::Application.config.secret_token = 'f909eeceac462f39392abc4c02b39f333659df09cc586ee22b86e6e67122e6a24502f543b39d941aeb9f7d612c4f310b36be128a32dbc7ae8a37ca52e599d177'
