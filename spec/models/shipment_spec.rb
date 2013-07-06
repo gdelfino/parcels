8105df6dd9538476cef94e57b077cd9da5b8a8c0
Gustavo Delfino <gdelfino@mac.com>
2013-07-06 16:04:23 -0430

    additional Shipment specs placeholders

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -11,6 +11,12 @@ describe Shipment do
         shipment.errors[attr].should_not be_empty
       end
     end
-  end
 
+  end
+  it 'kg should be an integer larger or equal than 8kg'
+  it 'unit3kg should be a positive integer'
+  it 'unit5kg should be a positive integer'
+  it 'kg should be equal to 3 * unit3kg + 5 * unit5kg'
+  it 'setting kg should automatically set the optimum unit3kg and unit5kg'
+  it 'a non optimal combination of kg, unit3kg and unit5kg should be invalid'
 end
