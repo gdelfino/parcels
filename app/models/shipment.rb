cff4c3e531cb6cd6036fe56f51e38ec8b7fb2d5e
Gustavo Delfino <gdelfino@mac.com>
2013-07-08 05:17:43 -0430

    model refactor

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,16 +1,18 @@
 class Shipment < ActiveRecord::Base
   attr_accessible :kg, :units3kg, :units5kg
-  validate :kg_units3kg_and_units5kg_are_consistent,
-           :kg_units3kg_and_units5kg_are_optimal
+
   validates :kg, :units3kg, :units5kg, presence: true
   validates :units3kg, :units5kg, numericality: {greater_than_or_equal_to: 0, only_integer: true}
   validates :kg,                  numericality: {greater_than_or_equal_to: 8, only_integer: true}
 
-  def kg= val
+  validate :kg_units3kg_and_units5kg_are_consistent,
+           :kg_units3kg_and_units5kg_are_optimal
+
+  def kg= mass_in_kg
     # This algorithm was deduced with the help of Wolfram Mathematica
-    self.units3kg = (2 * val - 10) % 5
-    self.units5kg = ([-3,-9,0,-6,-12].rotate(val-8)[0] + val)/5
-    super val
+    self.units3kg = calc_units3kg(mass_in_kg)
+    self.units5kg = calc_units5kg(mass_in_kg)
+    super mass_in_kg
   end
 
   def kg_units3kg_and_units5kg_are_consistent
@@ -22,9 +24,19 @@ class Shipment < ActiveRecord::Base
 
   def kg_units3kg_and_units5kg_are_optimal
     return unless kg && units3kg && units5kg
-    if units3kg != (2 * kg - 10) % 5
+    if units3kg != calc_units3kg(kg)
       errors.add(:base, "non-optimal units3kg & units5kg combination")
     end
+  end
+
+  private
+
+  def calc_units3kg(mass_in_kg)
+    (2 * mass_in_kg - 10) % 5
+  end
+
+  def calc_units5kg(mass_in_kg)
+    ([-3,-9,0,-6,-12].rotate(mass_in_kg - 8)[0] + mass_in_kg)/5
   end
 
 end
