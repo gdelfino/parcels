eeb20398bc3d142a11ac45be41696d2b5877d667
Gustavo Delfino <gdelfino@mac.com>
2013-07-07 21:01:09 -0430

    Default controller spec no longer failing.

--- spec/controllers/shipments_controller_spec.rb
+++ spec/controllers/shipments_controller_spec.rb
@@ -23,7 +23,7 @@ describe ShipmentsController do
   # This should return the minimal set of attributes required to create a valid
   # Shipment. As you add validations to Shipment, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "kg" => "1" } }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:shipment) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
